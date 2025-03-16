4970e0c050215956dde58f221c55ec4298b1183a
Yuma Antoine Decaux <yuma@astreos.space>
2025-03-16 15:30:14 +1000

    printing id on 99 modulo

--- Sources/SwiftHorizons/SwiftHorizons+parsing.swift
+++ Sources/SwiftHorizons/SwiftHorizons+parsing.swift
@@ -114,11 +114,8 @@ extension SwiftHorizons {
                 950: "Pluto"
             ]
             
-            let planetId = idString
-            let startP = planetId.index(planetId.startIndex, offsetBy: 2)
-            let endP = planetId.index(planetId.startIndex, offsetBy: 3)
-            let postFix = Int(planetId[startP..<endP])!
-            if postFix == 99 || id < 0 || id > 99999 || id == 0 { // Planet IDs usually end in 99
+            if id % 100 == 99 || id < 0 || id > 99999 || id == 0 { // Planet IDs usually end in 99
+                print("Found id\(id)")
                 output.append(MB(id: id, name: name, designation: designation, aliases: aliases))
             } else if id < 1000 && id > 299 && id % 100 != 99 {
                 let planet = planets[id/100]!
