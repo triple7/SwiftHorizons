3a553fd015660bcbb096fd0e320b394c8ce38d70
Yuma Antoine Decaux <yuma@astreos.space>
2025-03-23 19:48:50 +1000

    typo from some bad copy paste

--- Sources/SwiftHorizons/SwiftHorizons+parsing.swift
+++ Sources/SwiftHorizons/SwiftHorizons+parsing.swift
@@ -31,8 +31,7 @@ extension SwiftHorizons {
     internal func getElementBlock(text: String) -> [String] {
 
         let start = text.components(separatedBy: "$$SOE\n").last!
-        print("start: \(start)")
-        let soe = "$$SOE\n\(start.components(separatedBy: "EOE").first!)"
+        let soe = start.components(separatedBy: "$$EOE").first!
         var elementBlock = soe.components(separatedBy: "\n")
         elementBlock.removeFirst()
         elementBlock.removeLast()
