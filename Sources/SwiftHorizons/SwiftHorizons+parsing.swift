890b85e3142f7a8bb65bd5e68792c707e024d0e5
Yuma Antoine Decaux <yuma@astreos.space>
2025-03-24 09:16:05 +1000

    shifted all blocks to triplets

--- Sources/SwiftHorizons/SwiftHorizons+parsing.swift
+++ Sources/SwiftHorizons/SwiftHorizons+parsing.swift
@@ -58,27 +58,27 @@ extension SwiftHorizons {
             let ecqrinBlock = orbitalBlock[i*5+1].replacingOccurrences(of: "= ", with: "=").replacingOccurrences(of: " =", with: "=").components(separatedBy: " ")
             print("ecqrin")
             print(ecqrinBlock)
-            let ec = Double(ecqrinBlock[1].components(separatedBy: "=")[1])!
-            let qr = Double(ecqrinBlock[2].components(separatedBy: "=")[1])!
-            let inc = Double(ecqrinBlock[3].components(separatedBy: "=")[1])!
+            let ec = Double(ecqrinBlock[0].components(separatedBy: "=")[1])!
+            let qr = Double(ecqrinBlock[1].components(separatedBy: "=")[1])!
+            let inc = Double(ecqrinBlock[2].components(separatedBy: "=")[1])!
             let omwtpBlock = orbitalBlock[i*5+2].replacingOccurrences(of: "= ", with: "=").replacingOccurrences(of: " =", with: "=").replacingOccurrences(of: "  ", with: "").components(separatedBy: " ")
             print("omwtp block")
             print(omwtpBlock)
-            let om = Double(omwtpBlock[1].components(separatedBy: "=")[1])!
-            let w = Double(omwtpBlock[2].components(separatedBy: "=")[1])!
-            let tp = Double(omwtpBlock[4])!
+            let om = Double(omwtpBlock[0].components(separatedBy: "=")[1])!
+            let w = Double(omwtpBlock[1].components(separatedBy: "=")[1])!
+            let tp = Double(omwtpBlock[2])!
             let amataBlock = orbitalBlock[i*5+3].replacingOccurrences(of: " = ", with: "=").replacingOccurrences(of: "= ", with: "=").replacingOccurrences(of: "  ", with: "").components(separatedBy: " ")
             print("amata block")
             print(amataBlock)
-            let n = Double(amataBlock[1].components(separatedBy: "=")[1])!
-            let ma = Double(amataBlock[2].components(separatedBy: "=")[1])!
-            let ta = Double(amataBlock[3].components(separatedBy: "=")[1])!
+            let n = Double(amataBlock[0].components(separatedBy: "=")[1])!
+            let ma = Double(amataBlock[1].components(separatedBy: "=")[1])!
+            let ta = Double(amataBlock[2].components(separatedBy: "=")[1])!
             let Aadapr = orbitalBlock[i*5+4].replacingOccurrences(of: " = ", with: "=").replacingOccurrences(of: "= ", with: "=").replacingOccurrences(of: " =", with: "=").replacingOccurrences(of: "  ", with: "").components(separatedBy: " ")
             print("Aadaapr block")
             print(Aadapr)
-            let A = Double(Aadapr[1].components(separatedBy: "=")[1])!
-            let ad = Double(Aadapr[2].components(separatedBy: "=")[1])!
-            let apr = Double(Aadapr[3].components(separatedBy: "=")[1])!
+            let A = Double(Aadapr[0].components(separatedBy: "=")[1])!
+            let ad = Double(Aadapr[1].components(separatedBy: "=")[1])!
+            let apr = Double(Aadapr[2].components(separatedBy: "=")[1])!
             ephemorbitals.append(OrbitalElements(epoch: epoch, eccentricity: ec, periapsisDistance: qr, inclination: inc, ascendingNode: om, argumentOfPeriapsis: w, timeOfPeriapsis: tp, meanMotion: n, meanAnomaly: ma, trueAnomaly: ta, semiMajorAxis: A, apoapsisDistance: ad, orbitalPeriod: apr))
         }
         print("epheOrbitals: \(ephemorbitals)")
