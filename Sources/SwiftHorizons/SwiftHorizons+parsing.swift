204f4c80da9a9bbf11beb18d92118d8e20a0fcd9
Yuma Antoine Decaux <yuma@astreos.space>
2025-04-12 09:39:22 +1000

    try last component of delimiter

--- Sources/SwiftHorizons/SwiftHorizons+parsing.swift
+++ Sources/SwiftHorizons/SwiftHorizons+parsing.swift
@@ -24,7 +24,7 @@ extension SwiftHorizons {
 
     internal func extractNewDate(text: String) -> (start: String, stop: String) {
         let asteriskDelimitor = "***********************************************************************\n"
-        let format = text.components(separatedBy: asteriskDelimitor)[1]
+        let format = text.components(separatedBy: asteriskDelimitor).last!
         let upToDate = format.components(separatedBy: "A.D.").last!
         let components = upToDate.components(separatedBy: " ")
         print("Got components")
