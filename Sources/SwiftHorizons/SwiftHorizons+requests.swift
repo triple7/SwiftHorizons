44f47e9b0ce4855cef3841a86b827bca0813e6f6
Yuma Antoine Decaux <yuma@astreos.space>
2025-04-15 15:37:31 +1000

    getting vectors target rectified dates instead of passing

--- Sources/SwiftHorizons/SwiftHorizons+requests.swift
+++ Sources/SwiftHorizons/SwiftHorizons+requests.swift
@@ -86,7 +86,6 @@ extension SwiftHorizons: URLSessionDelegate {
             let operation = DownloadOperation(session: URLSession.shared, dataTaskURL: request.getURL(stop: self.sampleTimeDays), completionHandler: { (data, response, error) in
                 if self.requestIsValid(message: "state vectors for \(object.name)", error: error, response: response) {
                     let text = String(decoding: data!, as: UTF8.self)
-                    print(text)
                     if text.contains("Matching small-bodies") {
                         // TODO: write logic to get either 1 or all records added
                         self.addSyslog(message: "ephemerus has multiple entries, skipping", logType: .Warning)
@@ -94,15 +93,11 @@ extension SwiftHorizons: URLSessionDelegate {
                         self.targets[object.id] = HorizonsTarget(name: object.name, id: object.id, designation: object.type.id, parent: object.parent, objectType: object.objectType)
                         self.downloaded.append(object)
                     } else if text.contains("No ephemeris for target"){
-                        let result = self.rectifyDate(text)
-                        if result == "FUTURE" {
-                            self.addSyslog(message: "Ephemerus is historical", logType: .FUTURE)
-                            if self.batch.isEmpty {
-                                if notify {
-                                    NotificationCenter.default.post(name: resetToEarthNotification, object: nil)
-                                }
-                            }
-                        }
+                        let rectified = self.extractNewDate(text: text)
+                        var newTarget = object
+                        newTarget.setTime(start: rectified.start, stop: rectified.stop)
+                        remainingObjects.insert(newTarget, at: 0)
+                        self.addSyslog(message: "Rectified dates for \(object.name) \(object.id) to \(rectified.start)", logType: .OK)
                     }else{
                         let target = self.parseSingleTarget(name: object.name, id: object.id, objectType: object.objectType, parent: object.parent, parameters: request.parameters, text: text, type: type, notify)
                         self.targets[object.id] = target
